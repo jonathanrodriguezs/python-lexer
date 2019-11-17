37fc4ea40af3d7aa75c6952703bde6ec11c8e006
Jonathan Rodriguez Sanchez <jonathan.rodriguezs@softtek.com>
2019-11-17 00:18:07 -0600

    fix: relative read program.txt

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
-import lexer_rules
 from ply.lex import lex
+import lexer_rules
+from os import path
 
-f = open('program.txt', 'r')
+from helpers import find_column
+
+script_dir = path.dirname(__file__)
+source_code = path.join(script_dir, 'program.txt')
+
+f = open(source_code, 'r')
 
 text = f.read()
 lexer = lex(module=lexer_rules)
 lexer.input(text)
-token = lexer.token()
 
-while token is not None:
+for token in lexer:
     print('{}: {}'.format(token.type, token.value))
-    token = lexer.token()
+    # print(token)
